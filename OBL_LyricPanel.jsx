ff9700bf7f5a2949834668073901c7f6c5d4808f
OblivionMGD <ephixa6022@gmail.com>
2020-07-25 00:51:24 +0900

    First Commit

---
+++ OBL_LyricPanel.jsx
@@ -0,0 +1,57 @@
+﻿function OBL_LyricPanel(thisObj){
+    var chooseBtn;
+    var clearBtn;
+    var lyric;
+
+    var lyricContent = "";
+    var lyricPanel = (thisObj instanceof Panel)? thisObj : new Window("palette", "OBL_LyricPanel", undefined, {resizeable : true});
+
+
+    var myToolsPanel = createUI(thisObj);
+    if(this instanceof Panel)
+        myToolsPanel.show();
+
+    function createUI(thisObj){
+
+        try{
+            var buttonGroup = lyricPanel.add("group", [10,10,150,60], 'buttonGroup');
+            chooseBtn = buttonGroup.add("button", [10, 10, 80, 40], "Load Lyric :");
+            clearBtn = buttonGroup.add("button", [10, 10, 80, 40], "clear");
+
+        }
+        catch(e){
+            alert(e);
+        }
+
+        return myPanel;
+    }
+
+    chooseBtn.onClick = function LoadLyric(){
+        filename = File.openDialog("Choose File");
+        fileObj = new File(filename);
+        var flag = 0;
+
+
+        if(fileObj.open("r")){
+            while(!fileObj.eof){
+                lyricContent += fileObj.readln() + "\r\n";
+            }
+            fileObj.close();
+            flag = 1;
+        }
+        
+        lyric = lyricPanel.add("edittext", [10,50,300,250], lyricContent, {multiline: true});
+        
+    }
+    clearBtn.onClick = function clearlyric (){
+        lyric.parent.remove(lyric);
+        lyricPanel.layout.layout(true);
+        lyricPanel.layout.resize();
+    }
+
+    lyricPanel.onResize = function resize() {
+        lyricPanel.layout.resize(); 
+    }
+}
+
+OBL_LyricPanel(this);
---
+++ readme.md
@@ -0,0 +1,13 @@
+# OBL_LyricPanel
+## Overview
+After effects内に歌詞などのテキストを表示するスクリプトです。コピペなどにお使いください。
+Load Lyric:でテキストファイルを参照できます。
+Clearを押してパネルを非表示にします。
+
+## Installation
+下記のパスにOBL_LyricPanel.jsxを配置してください。
+
+```
+Win: C:\Program Files\Adobe\Adobe After Effects 2020\Support Files\Scripts\ScriptUI Panels
+Mac: /Application/Adobe After Effects 2020/Scripts/ScriptUI Panels
+```
